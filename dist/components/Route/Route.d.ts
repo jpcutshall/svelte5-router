ac9563d8a5bf63fde052223ca5e8d515a7e6ba1a
Keuller Magalhaes <keuller.magalhae@gmail.com>
2025-01-27 09:18:35 +0000

    feat: Updated test application

--- dist/components/Route/Route.d.ts
+++ dist/components/Route/Route.d.ts
@@ -1,4 +1,30 @@
 import type { Component, Snippet } from "svelte";
+export type AsyncComponent = () => Promise<{
+    default: Component<any, Record<string, unknown>>;
+}>;
+/**
+ * Receives a dynamic import to load  a component asynchronously
+ * @example
+ * ```svelte
+ * <script lang="ts">
+ * const AboutRoute = dynamic(import("./routes/About.svelte"));
+ * </script>
+ *
+ * <Router>
+ *   <Route path="/about" component={AboutRoute} />
+ * </Router>
+ * ```
+ * @param p Promuse
+ * @returns an instance of an AsyncComponent
+ */
+export declare function dynamic(p: Promise<unknown>): AsyncComponent;
+/**
+ * From Svelte 5 on, components are functions, so this is guard function
+ * that guarantees it's an dynamic component rather than a regular one.
+ * @param fn component
+ * @returns
+ */
+export declare function isAsync(fn: Function | undefined): fn is AsyncComponent;
 export type RouteProps = {
     path?: string;
     component?: Component<any, Record<string, any>>;
