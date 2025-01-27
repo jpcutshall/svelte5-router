746b0ff5fc81bde6c5d423558c2fae8a0425daf0
Keuller Magalhaes <keuller.magalhae@gmail.com>
2025-01-27 09:25:12 +0000

    fix: typo

--- src/components/Route/Route.ts
+++ src/components/Route/Route.ts
@@ -16,7 +16,7 @@ export type AsyncComponent = () => Promise<{
  *   <Route path="/about" component={AboutRoute} />
  * </Router>
  * ```
- * @param p Promuse
+ * @param p an instance of Promise
  * @returns an instance of an AsyncComponent
  */
 export function dynamic(p: Promise<unknown>): AsyncComponent {
