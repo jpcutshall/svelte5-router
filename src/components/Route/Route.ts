bef2d29c1fe7e5fa7abe9c21462291d39e83cc50
Keuller Magalhaes <keuller.magalhae@gmail.com>
2025-01-27 08:58:15 +0000

    Add Support to load Async Svelte component
        **What's changed?**
    
        - Add support for import components dynamically
        - Map those async components via Route component
    
        This PR addresses an important feature for dynamic imports, which allow us to split the app bundle in chunks.

--- src/components/Route/Route.ts
+++ src/components/Route/Route.ts
@@ -1,8 +1,42 @@
 import type { Component, Snippet } from "svelte";
 
-type AsyncSvelteComponent = () => Promise<{
-  default: Component;
-}>;
+export type AsyncComponent = {
+  (): Promise<{ default: Component<any, Record<string, unknown>>; }>
+};
+
+/**
+ * Receives a dynamic import to load  a component asynchronously
+ * @example
+ * ```svelte
+ * <script lang="ts">
+ * const AboutRoute = dynamic(import("./routes/About.svelte"));
+ * </script>
+ *
+ * <Router>
+ *   <Route path="/about" component={AboutRoute} />
+ * </Router>
+ * ```
+ * @param p Orimuse
+ * @returns an instnace of an AsyncComponent
+ */
+export function dynamic(p: Promise<unknown>): AsyncComponent {
+  const loader = (function loaderComponent() {
+    return p as Promise<{ default: Component<any, Record<string, unknown>>; }>
+  });
+  loader.prototype.name = "dynComponent";
+  return loader;
+}
+
+/**
+ * From Svelte 5 on, components are functions, so this is guard function
+ * that guarantees it's an dynamic component rather than a regular one.
+ * @param fn component
+ * @returns
+ */
+export function isAsync(fn: Function | undefined): fn is AsyncComponent {
+  if (!fn) return false;
+  return "name" in fn.prototype && fn.prototype.name === "dynComponent";
+}
 
 export type RouteProps = {
   path?: string;
