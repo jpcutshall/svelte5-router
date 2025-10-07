87eee0dd88ddd7f20124aa661746d645ddf698f5
Piotr Sarna <piotr@piksar.eu>
2025-10-07 17:38:09 +0200

    Remove tmp .svelte-kit files

--- .svelte-kit/__package__/components/Route/Route.svelte.d.ts
+++
@@ -1,5 +0,0 @@
-import type { RouteProps } from "./Route.js";
-declare const Route: import("svelte").Component<RouteProps, {}, "">;
-type Route = ReturnType<typeof Route>;
-export default Route;
-//# sourceMappingURL=Route.svelte.d.ts.map
