3a96203e9450c3408496ca393a6d48d5125881b3
Jacob Cutshall <93149732+JacobCutshall@users.noreply.github.com>
2026-02-13 19:51:08 -0500

    chore: remove generated .svelte-kit directory

--- .svelte-kit/__package__/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as Link } from "./components/Link/Link.svelte";
-export { default as Route } from "./components/Route/Route.svelte";
-export { default as Router } from "./components/Router/Router.svelte";
-export { link, links } from "./lib/actions.js";
-export { useHistory, useLocation, useRouter } from "./lib/contexts.js";
-export { navigate, listen } from "./lib/history.js";
-export { dynamic } from "./components/Route/Route";
-//# sourceMappingURL=index.d.ts.map
